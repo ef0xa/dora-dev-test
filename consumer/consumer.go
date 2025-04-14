58f579e486db8541301db5ef5604daa4fe77175d
efron <efron.dev@gmail.com>
2025-04-14 08:44:13 -0700

    fixup / commentary

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -8,81 +8,50 @@ import (
 	"log/slog"
 
 	"dora-dev-test/data"
-	"dora-dev-test/datastore"
 
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
-type Consumer interface {
-	Start(ctx context.Context)
-	Save(ctx context.Context, tick data.Tick) error
-	Stop() error
-}
-
-type consumer struct {
-	client *kgo.Client
-	ds     datastore.DataStore
-	cancel context.CancelFunc
-	err    error
-}
-
-func (c *consumer) Save(ctx context.Context, tick data.Tick) error {
-	return c.ds.SaveTick(ctx, tick)
-}
-
-func (c *consumer) Start(parent context.Context) {
-	ctx, cancel := context.WithCancel(parent)
-	c.cancel = cancel
-
-	go c.start(ctx)
-}
-
-func (c *consumer) start(ctx context.Context) (err error) {
-	defer func() { // save the error so we can also surface it during stop()
-		if err != nil {
-			c.err = err
-		}
-	}()
-	c.client.AddConsumeTopics("incoming_prices")
+// Start the consumer. This blocks until the context is canceled; run it in a goroutine.
+func Start(ctx context.Context,
+	kafka *kgo.Client,
+	saveTick func(ctx context.Context, tick data.Tick) error,
+	topics ...string,
+) error {
+	kafka.AddConsumeTopics(topics...)
+	var tick []data.Tick
 	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			slog.Debug("polling records")
-			fetches := c.client.PollRecords(ctx, 80)
-			var err error
-			for _, e := range fetches.Errors() {
-				err = errors.Join(err, fmt.Errorf("partition %d: topic %s: %w", e.Partition, e.Topic, e.Err))
-			}
-			if err != nil {
-				slog.Error("got an error", "err", err.Error())
+		tick, err := poll(ctx, kafka, tick[:0])
+		if err != nil {
+			return err
+		}
+		for _, t := range tick {
+			if err := saveTick(ctx, t); err != nil {
 				return err
 			}
-			for iter := fetches.RecordIter(); !iter.Done(); {
-				rec := iter.Next()
-				var tick data.Tick
-				if err := json.Unmarshal(rec.Value, &tick); err != nil {
-					return fmt.Errorf("unmarshal %q to %T: %w", rec.Value, tick, err)
-				}
-				slog.Debug("got tick", "tick", tick)
-
-				if err := c.Save(ctx, tick); err != nil {
-					return err
-				}
-
-			}
 		}
 	}
 }
 
-func (c *consumer) Stop() error {
-	if c.cancel != nil {
-		c.cancel()
+func poll(ctx context.Context, kafka *kgo.Client, buf []data.Tick) ([]data.Tick, error) {
+	slog.Debug("polling records")
+	fetches := kafka.PollRecords(ctx, 80)
+	var err error
+	for _, e := range fetches.Errors() {
+		err = errors.Join(err, fmt.Errorf("partition %d: topic %s: %w", e.Partition, e.Topic, e.Err))
 	}
-	return c.err
-}
+	if err != nil {
+		slog.Error("got an error", "err", err.Error())
+		return nil, err
+	}
 
-func NewConsumer(client *kgo.Client, ds datastore.DataStore) Consumer {
-	return &consumer{client: client, ds: ds}
+	for iter := fetches.RecordIter(); !iter.Done(); {
+		rec := iter.Next()
+		var tick data.Tick
+		if err := json.Unmarshal(rec.Value, &tick); err != nil {
+			return nil, err
+		}
+		buf = append(buf, tick)
+	}
+	return buf, nil
 }
